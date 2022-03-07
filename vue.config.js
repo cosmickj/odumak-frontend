12ee3ab28652f597955355eeca5ba4ec32ee3b53
cosmicKj <kyungjun1213@gmail.com>
2022-03-07 19:40:18 +0900

    Feat: 누적 출석 현황 reset & 빌드 모드 추가

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: "오두막",
+    themeColor: "#005d9a",
+  },
   devServer: {
     client: {
       overlay: false,
     },
-  },
-  pwa: {
-    name: "오두막",
-    themeColor: "#005d9a",
   },
 });
