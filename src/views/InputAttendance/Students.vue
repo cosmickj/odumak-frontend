b18159e6991a0d64fb86b173546c91bdf066337c
cosmicKj <kyungjun1213@gmail.com>
2022-03-25 11:09:01 +0900

    Feat: 출석 입력 화면 CSS 정리

--- src/views/InputAttendance/Students.vue
+++ src/views/InputAttendance/Students.vue
@@ -1,78 +1,86 @@
-<template>
-  <template v-if="students">
-    <div class="teacher">
-      <div>선생님께서는 어디서 예배하셨나요?</div>
-      <label for="teacher-online">온라인</label>
-      <input
-        v-model="teacherAttendance"
-        id="teacher-online"
-        type="radio"
-        name="teacher-attendance"
-        value="online"
-      />
-      <label for="teacher-offline">현장예배</label>
-      <input
-        v-model="teacherAttendance"
-        id="teacher-offline"
-        type="radio"
-        name="teacher-attendance"
-        value="offline"
-      />
-    </div>
-    <div v-for="(student, i) in students" :key="i" class="student">
-      <div class="student__name">{{ student.name }}</div>
-      <div class="student__birth">{{ student.birth }}</div>
-      <div class="student__attendance">
-        <input
-          type="radio"
-          :name="student.name"
-          :id="`${student.name}__offline`"
-          value="offline"
-          v-model="students[i].attendance"
-        />
-        <label
-          :for="`${student.name}__offline`"
-          style="background: #4caf50; color: #28334aff"
-        >
-          <span>현장</span>
-        </label>
-
-        <input
-          type="radio"
-          :name="student.name"
-          :id="`${student.name}__online`"
-          value="online"
-          v-model="students[i].attendance"
-        />
-        <label
-          :for="`${student.name}__online`"
-          style="background: #fbc02d; color: #28334aff"
-        >
-          <span>온라인</span>
-        </label>
-
-        <input
-          type="radio"
-          :name="student.name"
-          :id="`${student.name}__absence`"
-          value="absence"
-          v-model="students[i].attendance"
-        />
-        <label
-          :for="`${student.name}__absence`"
-          style="background: #ff4032; color: #28334aff"
-        >
-          <span>결석</span>
-        </label>
-      </div>
-    </div>
-
-    <Button
-      label="다음"
-      class="p-button-success p-button-raised btn-block"
-      @click="moveStage('teacher')"
+<template v-if="students">
+  <!-- 선생님 출석체크 -->
+  <div class="attendance teacher">
+    <div>선생님께서는 어디서 예배하셨나요?</div>
+    <input
+      type="radio"
+      id="teacher-online"
+      value="online"
+      v-model="teacher"
+      class="attendance__input"
     />
-  </template>
+    <label
+      for="teacher-online"
+      class="attendance__label attendance__label--online"
+    >
+      <span>온라인</span>
+    </label>
+    <input
+      type="radio"
+      id="teacher-offline"
+      value="offline"
+      v-model="teacher"
+      class="attendance__input"
+    />
+    <label
+      for="teacher-offline"
+      class="attendance__label attendance__label--offline"
+    >
+      <span>현장</span>
+    </label>
+  </div>
+
+  <!-- 학생 출석체크 -->
+  <div v-for="(student, i) in students" :key="i" class="attendance student">
+    <div class="student__name">{{ student.name }}</div>
+    <div class="student__birth">{{ student.birth }}</div>
+    <input
+      type="radio"
+      :id="`absence-${student.name}`"
+      value="absence"
+      v-model="students[i].attendance"
+      class="attendance__input"
+    />
+    <label
+      :for="`absence-${student.name}`"
+      class="attendance__label attendance__label--absence"
+    >
+      <span>결석</span>
+    </label>
+    <input
+      type="radio"
+      :id="`online-${student.name}`"
+      value="online"
+      v-model="students[i].attendance"
+      class="attendance__input"
+    />
+    <label
+      :for="`online-${student.name}`"
+      class="attendance__label attendance__label--online"
+    >
+      <span>온라인</span>
+    </label>
+    <input
+      type="radio"
+      :id="`offline-${student.name}`"
+      value="offline"
+      v-model="students[i].attendance"
+      class="attendance__input"
+    />
+    <label
+      :for="`offline-${student.name}`"
+      class="attendance__label attendance__label--offline"
+    >
+      <span>현장</span>
+    </label>
+  </div>
+
+  <!-- 버튼 영역 -->
+  <Button
+    label="다음"
+    class="p-button-success p-button-raised btn-block"
+    @click="moveStage('teacher')"
+  />
 </template>
 
 <script lang="ts">
@@ -81,15 +89,15 @@ import { useStore } from "vuex";
 import Student from "@/types/Student";
 
 export default defineComponent({
-  name: "InputAttendanceStudents",
+  name: "InputAttendance",
   setup() {
     const store = useStore();
-    const teacherAttendance = ref("");
+    const teacher = ref("offline");
     const students = computed<Student[]>(() => store.state.attendance.students);
     const moveStage = (stage: string) =>
       store.commit("attendance/SET_STAGE", stage);
     return {
-      teacherAttendance,
+      teacher,
       students,
       moveStage,
     };
@@ -98,35 +106,36 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.teacher {
+.attendance {
   padding: 1rem;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-evenly;
   align-items: center;
   margin: 10px 0;
   border-radius: 3px;
   box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.3);
   background: #efefef95;
 }
-.student {
-  padding: 1rem;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  margin: 10px 0;
-  border-radius: 3px;
-  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.3);
-  background: #efefef95;
+.attendance__label--offline {
+  background-color: #4caf50;
+  color: #28334aff;
 }
-.student__attendance {
-  display: flex;
+.attendance__label--online {
+  background-color: #fbc02d;
+  color: #28334aff;
 }
-input[type="radio"] {
+.attendance__label--absence {
+  background-color: #ff4032;
+  color: #28334aff;
+}
+/* input[type="radio"] { */
+.attendance__input {
   display: none;
   height: 0;
   width: 0;
 }
-input[type="radio"] ~ label {
+/* input[type="radio"] ~ label { */
+.attendance__label {
   height: 48px;
   width: 48px;
   font-size: 0.6rem;
