b7f352fff3f3209d3351c5df853bd5b7898ff21c
cosmicKj <kyungjun1213@gmail.com>
2022-05-02 20:29:18 +0900

    Feat: 로그인 & 회원가입 에러처리 UX 개선

--- src/store/account.ts
+++ src/store/account.ts
@@ -27,7 +27,7 @@ export const account: Module<AccountState, RootState> = {
   },
 
   actions: {
-    async signup(_context, { email, password, name }) {
+    async signup(context, { email, password, name }) {
       try {
         const signupResponse = await createUserWithEmailAndPassword(auth, email, password);
         await updateProfile(signupResponse.user, { displayName: name });
@@ -36,7 +36,8 @@ export const account: Module<AccountState, RootState> = {
         throw new Error("could not complete signup");
       }
     },
-    async createUser(_context, { uid, grade, group }) {
+
+    async createUser(context, { uid, grade, group }) {
       try {
         await setDoc(doc(db, "users", uid), {
           grade,
@@ -47,10 +48,12 @@ export const account: Module<AccountState, RootState> = {
         throw new Error("could not complete creating a user");
       }
     },
+
     async login({ commit, dispatch }, { email, password }) {
       try {
         const loginResponse = await signInWithEmailAndPassword(auth, email, password);
         const fetchUserResponse = await dispatch("fetchUser", loginResponse.user.uid);
+
         commit("SET_USER", {
           name: loginResponse.user.displayName,
           email: loginResponse.user.email,
@@ -61,6 +64,7 @@ export const account: Module<AccountState, RootState> = {
         throw new Error("could not complete login");
       }
     },
+
     async fetchUser(_context, uid) {
       const docRef = doc(db, "users", uid);
       const docSnap = await getDoc(docRef);
@@ -70,6 +74,7 @@ export const account: Module<AccountState, RootState> = {
         console.log("No such document!");
       }
     },
+
     async logout({ commit }) {
       await signOut(auth);
       commit("SET_USER", null);
--- src/views/account/AccountSignup.vue
+++ src/views/account/AccountSignup.vue
@@ -12,17 +12,15 @@
       </div>
 
       <div class="mx-7 mb-2">
-        <div>
-          <div class="flex overflow-auto">
-            <span
-              class="border-1 border-400 border-round mr-2 px-2"
-              v-for="(email, i) in emails"
-              :key="i"
-              @click="appendEmail(email)"
-            >
-              {{ email }}
-            </span>
-          </div>
+        <div class="flex overflow-auto">
+          <span
+            class="border-1 border-400 border-round mr-2 px-2"
+            v-for="(email, i) in emails"
+            :key="i"
+            @click="appendEmail(email)"
+          >
+            {{ email }}
+          </span>
         </div>
       </div>
 
@@ -35,9 +33,7 @@
           required
         >
           <template #footer>
-            <div class="mt-3">
-              영문 대소문자+숫자를 조합하여 8자리 이상 입력하세요.
-            </div>
+            <div class="mt-3">영문 대소문자+숫자를 조합하여 8자리 이상 입력하세요.</div>
           </template>
         </Password>
       </div>
@@ -46,10 +42,15 @@
         <Password
           v-model="confirmedPassword"
           class="w-full"
+          toggleMask
           :feedback="false"
           placeholder="비밀번호를 한 번 더 입력하세요."
           required
         />
+      </div>
+
+      <div v-if="isPasswordSame && confirmedPassword" class="mx-7 mb-2">
+        <span class="text-pink-500">새 비밀번호가 일치하지 않습니다.</span>
       </div>
 
       <div class="mx-7 mb-2">
@@ -62,26 +63,14 @@
         />
       </div>
 
-      <div
-        class="mx-7 mb-2 flex justify-content-between align-items-center text-lg"
-      >
+      <div class="mx-7 mb-2 flex justify-content-between align-items-center text-lg">
         <div class="flex-shrink-0">
-          <RadioButton
-            v-model="signupForm.grade"
-            id="third-grade"
-            name="grade"
-            value="3"
-          />
+          <RadioButton v-model="signupForm.grade" id="third-grade" name="grade" value="3" />
           <label class="ml-2" for="third-grade">3학년</label>
         </div>
 
         <div class="flex-shrink-0">
-          <RadioButton
-            v-model="signupForm.grade"
-            id="forth-grade"
-            name="grade"
-            value="4"
-          />
+          <RadioButton v-model="signupForm.grade" id="forth-grade" name="grade" value="4" />
           <label class="ml-2" for="forth-grade">4학년</label>
         </div>
 
@@ -97,16 +86,17 @@
       </div>
 
       <div class="mx-7 my-3">
-        <Button
-          class="p-button-warning w-full"
-          label="회원가입"
-          type="submit"
-        ></Button>
+        <Button type="submit" class="p-button-warning w-full justify-content-center">
+          <span v-if="!isLoading">회원가입</span>
+          <i v-else class="pi pi-spin pi-spinner"></i>
+        </Button>
       </div>
 
-      <div
-        class="mx-7 mt-4 mb-2 flex justify-content-evenly align-items-center"
-      >
+      <div v-if="!isAllFilled" class="mx-7 my-3">
+        <span class="text-pink-500">위 입력사항을 모두 입력해주세요.</span>
+      </div>
+
+      <div class="mx-7 mt-4 mb-2 flex justify-content-evenly align-items-center">
         <span>계정이 있으신가요?</span>
         <router-link :to="{ name: 'AccountLogin' }">
           <span class="text-yellow-500 font-bold">로그인</span>
@@ -117,12 +107,63 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from "vue";
+import { computed, reactive, ref } from "vue";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 import RadioButton from "primevue/radiobutton";
 import Dropdown from "primevue/dropdown";
 import Password from "primevue/password";
+
+const store = useStore();
+const router = useRouter();
+
+const initSignupForm = {
+  email: "",
+  password: "",
+  name: "",
+  grade: "3",
+  group: "",
+};
+const signupForm = reactive({ ...initSignupForm });
+
+const isLoading = ref(false);
+const confirmedPassword = ref("");
+const isPasswordSame = computed(() => {
+  if (signupForm.password === confirmedPassword.value) return false;
+  else return true;
+});
+const isAllFilled = ref(true);
+
+const onSubmit = async () => {
+  if (!Object.values(signupForm).every((value) => value)) {
+    isAllFilled.value = false;
+    return false;
+  }
+
+  if (isPasswordSame.value) {
+    return false;
+  }
+
+  try {
+    isLoading.value = true;
+
+    const signupResult = await store.dispatch("account/signup", signupForm);
+    await store.dispatch("account/createUser", {
+      uid: signupResult.user.uid,
+      ...signupForm,
+    });
+    await store.dispatch("account/login", {
+      email: signupForm.email,
+      password: signupForm.password,
+    });
+
+    router.push({ name: "AppHome" });
+  } catch (error) {
+    isAllFilled.value = true;
+    isLoading.value = false;
+    console.log(error);
+  }
+};
 
 const group = [
   { name: "1반", value: "1" },
@@ -136,25 +177,7 @@ const group = [
   { name: "9반", value: "9" },
   { name: "10반", value: "10" },
 ];
-const emails = [
-  "@gmail.com",
-  "@naver.com",
-  "@hotmail.com",
-  "@yahoo.com",
-  "@outlook.com",
-];
-const initSignupForm = {
-  email: "",
-  password: "",
-  name: "",
-  grade: "3",
-  group: "",
-};
-const signupForm = reactive({ ...initSignupForm });
-const confirmedPassword = ref("");
-
-const store = useStore();
-const router = useRouter();
+const emails = ["@gmail.com", "@naver.com", "@hotmail.com", "@yahoo.com", "@outlook.com"];
 
 const appendEmail = (email: string) => {
   const asperand = signupForm.email.indexOf("@");
@@ -163,23 +186,6 @@ const appendEmail = (email: string) => {
     signupForm.email = emailHead + email;
   } else {
     signupForm.email = signupForm.email + email;
-  }
-};
-
-const onSubmit = async () => {
-  try {
-    if (signupForm.password !== confirmedPassword.value) {
-      alert("비밀번호가 일치하지 않습니다.");
-    } else {
-      const signupResult = await store.dispatch("account/signup", signupForm);
-      await store.dispatch("account/createUser", {
-        uid: signupResult.user.uid,
-        ...signupForm,
-      });
-      router.push({ name: "AccountLogin" });
-    }
-  } catch (error) {
-    console.log(error);
   }
 };
 </script>
