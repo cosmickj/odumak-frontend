1c34dbf448710f8a00108ea9fa89e590d0a8a7cb
cosmicKj <kyungjun1213@gmail.com>
2022-05-11 08:01:32 +0900

    Feat: 네비게이션바 UI 변경

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="z-5 flex justify-content-evenly align-items-center h-5rem border-round shadow">
-    <router-link :to="{ name: 'AppHome' }">
-      <div class="p-2">
+  <div class="z-5 relative flex justify-content-evenly align-items-center h-6rem border-round shadow">
+    <div class="flex flex-column justify-content-center align-items-center">
+      <router-link :to="{ name: 'AppHome' }" class="p-2">
         <i class="pi pi-home" style="font-size: 2rem"></i>
-      </div>
-    </router-link>
+      </router-link>
+      <div>메인 화면</div>
+    </div>
 
-    <router-link :to="{ name: 'AttendanceInput' }">
-      <div class="bg-yellow-500 p-2 border-circle">
+    <div class="flex flex-column justify-content-center align-items-center">
+      <router-link :to="{ name: 'AttendanceInput' }" class="p-2 bg-yellow-500 border-round shadow">
         <i class="pi pi-plus" style="font-size: 2rem"></i>
-      </div>
-    </router-link>
+      </router-link>
+      <div>출석 입력</div>
+    </div>
 
-    <router-link :to="{ name: 'AppUser' }">
-      <div class="p-2">
+    <div class="flex flex-column justify-content-center align-items-center">
+      <router-link :to="{ name: 'AppUser' }" class="p-2">
         <i class="pi pi-user" style="font-size: 2rem"></i>
-      </div>
-    </router-link>
+      </router-link>
+      <div>내 정보</div>
+    </div>
   </div>
 </template>
