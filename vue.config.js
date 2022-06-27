2c9d714ae9a96cc16e12f51f55b4cc2854805d91
cosmicKj <kyungjun1213@gmail.com>
2022-06-27 23:41:04 +0900

    Feat: PWA 설정 작업

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  pwa: {
-    themeColor: "#f8f9fa",
-    backgroundColor: "#f8f9fa",
-    display: "standalone",
-    scope: "/",
-    startUrl: "/",
-    name: "오두막",
-    shortName: "오두막",
-    description: "영은교회 초등부 출석부 웹앱",
-  },
   devServer: {
     client: { overlay: false },
   },
