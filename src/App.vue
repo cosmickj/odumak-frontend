fbb567c20ca5199f4a35ea4ff36f72a8a4f0f7db
cosmicKj <kyungjun1213@gmail.com>
2022-07-05 23:56:40 +0900

    Feat: PWA 설치 안내 팝업 UI 작업

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,46 @@
 <template>
   <main class="min-h-screen flex">
-    <section class="flex-grow-1 flex justify-content-center align-items-center">
+    <section class="flex-grow-1 flex align-items-center justify-content-center">
       <div
-        class="wrapper relative flex-grow-1 flex flex-column justify-content-center w-screen h-screen surface-0 border-round"
+        class="wrapper relative flex-grow-1 flex justify-content-center flex-column w-screen h-screen surface-0 border-round"
       >
         <router-view></router-view>
+
+        <TheModalDownload :deferred-prompt="deferredPrompt" @dismiss="dismiss" @download="download" />
       </div>
     </section>
   </main>
 </template>
+
+<script setup lang="ts">
+import { inject, onMounted, ref } from "vue";
+import TheModalDownload from "@/components/TheModalDownload.vue";
+import type { VueCookies } from "vue-cookies/types/index";
+
+const $cookies = inject<VueCookies>("$cookies");
+const deferredPrompt = ref<Event | null>(null);
+
+onMounted(() => {
+  if (!$cookies?.get("download_later")) {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      deferredPrompt.value = e;
+    });
+    window.addEventListener("appinstalled", () => {
+      deferredPrompt.value = null;
+    });
+  }
+});
+
+const dismiss = () => {
+  setTimeout(() => (deferredPrompt.value = null), 800);
+  $cookies?.set("download_later", true, "1d");
+};
+const download = () => {
+  deferredPrompt.value?.prompt();
+  deferredPrompt.value = null;
+};
+</script>
 
 <style scoped>
 @media (min-width: 450px) {
