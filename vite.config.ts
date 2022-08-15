2634acf62c2b73b5db0514562845a8dcff7ff7de
cosmicKj <kyungjun1213@gmail.com>
2022-08-16 08:44:31 +0900

    Feat: 빌드시 chunk 파일 이름 랜덤 생성하게 하기

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { hash } from './src/utils/hash';
 
 export default defineConfig({
+  plugins: [vue()],
   resolve: {
     alias: { '@': resolve(__dirname, './src') },
   },
-  plugins: [vue()],
   server: {
     host: true,
     port: 8080,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`,
+      },
+    },
   },
 });
