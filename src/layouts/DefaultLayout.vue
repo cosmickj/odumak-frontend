6ba197f49df7587e8ea7877c34c0a99d65291ee5
cosmicKj <kyungjun1213@gmail.com>
2022-08-21 14:49:09 +0900

    Feat: DefaultLayout 코드 정리

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,14 +1,11 @@
 <template>
-  <main class="min-h-screen flex items-center justify-center sm:bg-gray-200">
-    <section
-      v-if="account.isAuthReady"
-      class="flex items-center justify-center w-screen h-screen sm:max-w-xl sm:max-h-240 bg-white shadow-lg"
-    >
-      <div class="grow h-full flex flex-col justify-center">
-        <RouterView></RouterView>
+  <main class="h-screen flex sm:bg-gray-200 items-center justify-center">
+    <div class="w-full sm:max-w-xl h-full sm:max-h-240 bg-slate-100 shadow-lg">
+      <div v-if="account.isAuthReady" class="flex flex-col h-full">
+        <RouterView class="grow"></RouterView>
         <RouterView name="GlobalNavbar"></RouterView>
       </div>
-    </section>
+    </div>
   </main>
 </template>
 
