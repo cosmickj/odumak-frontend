666a4b00f6a2ba85985b8e77d605be7b73dc8406
cosmicKj <kyungjun1213@gmail.com>
2022-11-04 22:48:49 +0900

    Feat: 학생 추가 Dialog UI 변경

--- src/views/Admin/Student/components/AdminStudentDialog.vue
+++ src/views/Admin/Student/components/AdminStudentDialog.vue
@@ -3,28 +3,34 @@
     v-model:visible="dialog.status"
     modal
     maximizable
-    :breakpoints="{ '450px': '90vw' }"
+    :breakpoints="{ '1280px': '90vw', '450px': '90vw' }"
     @hide="handleHide"
   >
     <template #header>
       <div class="flex gap-x-4 items-center">
         <span class="text-xl">{{ dialog.label }}</span>
-        <Button
-          class="p-button-success"
-          label="학생 추가"
-          @click="handleSubmit('EDIT')"
-        />
-        <Button
-          class="p-button-warning"
-          label="제출하기"
-          @click="handleSubmit('EDIT')"
-        />
+        <Button class="p-button-success" label="행 추가" />
+        <Button class="p-button-warning" label="제출하기" />
       </div>
     </template>
 
-    <div class="flex gap-x-4 items-center">
-      <div>
-        <p>학년</p>
+    <!-- <div class="overflow-x-auto"> -->
+    <div class="grid gap-x-5 gap-y-3 grid-cols-custom w-fit">
+      <!-- Header -->
+      <p class="self-baseline">학년</p>
+      <p class="self-baseline">학급</p>
+      <p class="self-baseline">이름</p>
+      <p class="self-baseline">생년월일</p>
+      <p class="self-baseline">성별</p>
+      <p class="self-baseline">연락처(- 없이 입력)</p>
+      <p class="self-baseline">누구의 연락처인가요?</p>
+      <p class="self-baseline">주소</p>
+      <p class="self-baseline">초등부 처음 온 날</p>
+      <p class="self-baseline">비고</p>
+      <p class="self-baseline">삭제</p>
+
+      <!-- Row -->
+      <template v-for="i in 1" :key="i">
         <Dropdown
           v-model="selectedStudent.grade"
           :class="{ 'p-invalid': errors.grade.status }"
@@ -33,10 +39,7 @@
           option-value="value"
           placeholder="학년"
         />
-      </div>
 
-      <div>
-        <p>학반</p>
         <Dropdown
           v-model="selectedStudent.group"
           :class="{ 'p-invalid': errors.group.status }"
@@ -45,57 +48,13 @@
           option-value="value"
           :options="GROUP"
         />
-      </div>
 
-      <div>
-        <p>이름</p>
         <InputText
           v-model="selectedStudent.name"
           :class="{ 'p-invalid': errors.name.status }"
           placeholder="이름을 입력해주세요."
         />
-      </div>
 
-      <div>
-        <p class="flex items-center">
-          생년월일
-          <!-- <InputSwitch v-model="isChecked" class="ml-4" input-id="later" />
-            <label class="ml-1 cursor-pointer select-none" for="later">
-              다음에 입력할게요
-            </label> -->
-        </p>
-        <div class="flex">
-          <Dropdown
-            v-model="selectedBirthYear"
-            option-label="label"
-            option-value="value"
-            :disabled="isChecked"
-            :options="BIRTH_YEAR"
-            @change="handleBirthChange"
-          />
-
-          <Dropdown
-            v-model="selectedBirthMonth"
-            option-label="label"
-            option-value="value"
-            :disabled="isChecked"
-            :options="BIRTH_MONTH"
-            @change="handleBirthChange"
-          />
-
-          <Dropdown
-            v-model="selectedBirthDate"
-            option-label="label"
-            option-value="value"
-            :disabled="isChecked"
-            :options="BIRTH_DATE"
-            @change="handleBirthChange"
-          />
-        </div>
-      </div>
-
-      <div>
-        <p>성별</p>
         <div class="flex items-center">
           <RadioButton
             name="gender"
@@ -104,7 +63,6 @@
             v-model="selectedStudent.gender"
           />
           <label class="cursor-pointer" for="male"> 남 </label>
-
           <RadioButton
             name="gender"
             inputId="female"
@@ -113,50 +71,60 @@
           />
           <label class="cursor-pointer" for="female"> 여 </label>
         </div>
-      </div>
 
-      <div>
-        <p>연락처(- 없이 입력)</p>
+        <div class="flex items-center">
+          <Dropdown
+            v-model="selectedBirthYear"
+            option-label="label"
+            option-value="value"
+            :disabled="isChecked"
+            :options="BIRTH_YEAR"
+            @change="handleBirthChange"
+          />
+          <Dropdown
+            v-model="selectedBirthMonth"
+            option-label="label"
+            option-value="value"
+            :disabled="isChecked"
+            :options="BIRTH_MONTH"
+            @change="handleBirthChange"
+          />
+          <Dropdown
+            v-model="selectedBirthDate"
+            option-label="label"
+            option-value="value"
+            :disabled="isChecked"
+            :options="BIRTH_DATE"
+            @change="handleBirthChange"
+          />
+          <InputSwitch v-model="isChecked" class="ml-4" input-id="later" />
+          <label class="cursor-pointer select-none" for="later"> 나중에 </label>
+        </div>
+
         <InputMask
           v-model="selectedStudent.phone"
           mask="(999)-9999-9999"
           placeholder="(010)-0000-0000"
         />
-      </div>
 
-      <div>
-        <p>누구의 연락처인가요?</p>
         <InputText placeholder="ex) 엄마, 아빠 등등" />
-      </div>
 
-      <div>
-        <p>어디 사는 친구인가요?</p>
         <InputText
           v-model="selectedStudent.address"
           placeholder="주소를 입력해주세요."
         />
-      </div>
-
-      <div class="shrink-0">
-        <p>초등부를 처음 나온 날</p>
 
         <Calendar
           v-model="selectedStudent.registeredAt"
           date-format="yy년 mm월 dd일"
-          touchUI
         />
-      </div>
 
-      <div>
-        <p>비고</p>
         <InputText v-model="selectedStudent.remark" />
-      </div>
 
-      <div>
-        <p>삭제</p>
         <Button class="p-button-danger" label="X" />
-      </div>
+      </template>
     </div>
+    <!-- </div> -->
   </Dialog>
 </template>
 
@@ -207,5 +175,11 @@ const handleHide = () => emit('hide');
 <style>
 .p-dropdown-panel {
   overflow: hidden;
+}
+</style>
+
+<style scoped>
+.grid-cols-custom {
+  grid-template-columns: repeat(11, max-content);
 }
 </style>
