0fdf56df6cdb908d187468d031350dfa0d1f716f
cosmicKj <kyungjun1213@gmail.com>
2023-02-14 21:28:35 +0900

    Feat: 관리자 페이지에서 홈으로 돌아가는 라우터 제작

--- src/views/Admin/AdminContainer.vue
+++ src/views/Admin/AdminContainer.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="flex h-screen">
-    <div class="shrink-0 w-52 py-6 bg-black text-white">
-      <Image :src="logoSidebar" />
-      <RouterLink class="block my-2 px-8 py-2" :to="{ name: 'AdminStudent' }">
-        <i class="mr-1 pi pi-home"></i>
-        학생 관리하기
-      </RouterLink>
+    <div class="shrink-0 flex flex-col justify-between w-52 py-6 bg-black">
+      <div class="flex flex-col gap-1 text-white">
+        <Image :src="logoSidebar" />
 
-      <RouterLink class="block my-2 px-8 py-2" :to="{ name: 'AdminTeacher' }">
-        <i class="mr-1 pi pi-heart"></i>
-        교사 관리하기
-      </RouterLink>
+        <RouterLink class="px-8 py-2" :to="{ name: 'AdminStudent' }">
+          <i class="mr-1 pi pi-home"></i>
+          학생 관리하기
+        </RouterLink>
 
-      <RouterLink class="block my-2 px-8 py-2" :to="{ name: 'AdminUser' }">
-        <i class="mr-1 pi pi-users"></i>
-        가입자 관리하기
+        <RouterLink class="px-8 py-2" :to="{ name: 'AdminTeacher' }">
+          <i class="mr-1 pi pi-heart"></i>
+          교사 관리하기
+        </RouterLink>
+
+        <RouterLink class="px-8 py-2" :to="{ name: 'AdminUser' }">
+          <i class="mr-1 pi pi-users"></i>
+          가입자 관리하기
+        </RouterLink>
+      </div>
+
+      <RouterLink
+        class="px-8 py-2 text-white text-center"
+        :to="{ name: 'HomeView' }"
+      >
+        홈으로 돌아가기
       </RouterLink>
     </div>
 
