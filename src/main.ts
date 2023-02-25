3b6f31cdfb8aa02d359885a1000fd3bb269bc3b0
cosmicKj <kyungjun1213@gmail.com>
2023-02-25 23:49:29 +0900

    Feat: DataView 컴포넌트 등록 및 예시 코드 복붙

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ import App from '@/App.vue';
 
 import PrimeVue from 'primevue/config';
 import ToastService from 'primevue/toastservice';
-
 import VueCookies from 'vue-cookies';
 
 import { getCurrentUser } from '@/router';
@@ -26,9 +25,9 @@ const app = createApp(App);
 
 app.use(router);
 app.use(createPinia());
-app.use(VueCookies);
 app.use(PrimeVue);
 app.use(ToastService);
+app.use(VueCookies);
 
 // Waiting for Auth to be Ready
 (async () => {
@@ -53,46 +52,48 @@ app.use(ToastService);
 })();
 
 import Button from 'primevue/button';
-import Card from 'primevue/card';
 import Calendar from 'primevue/calendar';
+import Card from 'primevue/card';
 import Checkbox from 'primevue/checkbox';
 import Column from 'primevue/column';
 import DataTable from 'primevue/datatable';
+import DataView from 'primevue/dataview';
 import Dialog from 'primevue/dialog';
 import Dropdown from 'primevue/dropdown';
 import FileUpload from 'primevue/fileupload';
+import Image from 'primevue/image';
 import InputMask from 'primevue/inputmask';
-import InputText from 'primevue/inputtext';
 import InputSwitch from 'primevue/inputswitch';
+import InputText from 'primevue/inputtext';
 import MultiSelect from 'primevue/multiselect';
 import Password from 'primevue/password';
+import ProgressSpinner from 'primevue/progressspinner';
 import RadioButton from 'primevue/radiobutton';
 import SelectButton from 'primevue/selectbutton';
-import Tree from 'primevue/tree';
-import Image from 'primevue/image';
 import Toast from 'primevue/toast';
-import ProgressSpinner from 'primevue/progressspinner';
+import Tree from 'primevue/tree';
 
 app
   .component('Button', Button)
-  .component('Card', Card)
   .component('Calendar', Calendar)
+  .component('Card', Card)
   .component('Checkbox', Checkbox)
   .component('Column', Column)
   .component('DataTable', DataTable)
+  .component('DataView', DataView)
   .component('Dialog', Dialog)
   .component('Dropdown', Dropdown)
   .component('FileUpload', FileUpload)
+  .component('Image', Image)
   .component('InputMask', InputMask)
-  .component('InputText', InputText)
   .component('InputSwitch', InputSwitch)
+  .component('InputText', InputText)
   .component('MultiSelect', MultiSelect)
   .component('Password', Password)
+  .component('ProgressSpinner', ProgressSpinner)
   .component('RadioButton', RadioButton)
   .component('SelectButton', SelectButton)
-  .component('Tree', Tree)
   .component('Toast', Toast)
-  .component('Image', Image)
-  .component('ProgressSpinner', ProgressSpinner);
+  .component('Tree', Tree);
 
 app.mount('#app');
--- src/views/Attendance/Tracker/TrackerDaily.vue
+++ src/views/Attendance/Tracker/TrackerDaily.vue
@@ -1,5 +1,114 @@
 <template>
   <div>Hello World</div>
+
+  <!-- <DataView
+    :value="products"
+    :layout="layout"
+    :paginator="true"
+    :rows="9"
+    :sortOrder="sortOrder"
+    :sortField="sortField"
+  >
+    <template #header>
+      <div class="grid grid-nogutter">
+        <div class="col-6" style="text-align: left">
+          <Dropdown
+            v-model="sortKey"
+            :options="sortOptions"
+            optionLabel="label"
+            placeholder="Sort By Price"
+            @change="onSortChange($event)"
+          />
+        </div>
+        <div class="col-6" style="text-align: right">
+          <DataViewLayoutOptions v-model="layout" />
+        </div>
+      </div>
+    </template>
+
+    <template #list="slotProps">
+      <div class="col-12">
+        <div class="product-list-item">
+          <img
+            src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png"
+            :alt="slotProps.data.name"
+          />
+          <div class="product-list-detail">
+            <div class="product-name">{{ slotProps.data.name }}</div>
+            <div class="product-description">
+              {{ slotProps.data.description }}
+            </div>
+            <Rating
+              :modelValue="slotProps.data.rating"
+              :readonly="true"
+              :cancel="false"
+            ></Rating>
+            <i class="pi pi-tag product-category-icon"></i
+            ><span class="product-category">{{ slotProps.data.category }}</span>
+          </div>
+          <div class="product-list-action">
+            <span class="product-price">${{ slotProps.data.price }}</span>
+            <Button
+              icon="pi pi-shopping-cart"
+              label="Add to Cart"
+              :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'"
+            ></Button>
+            <span
+              :class="
+                'product-badge status-' +
+                slotProps.data.inventoryStatus.toLowerCase()
+              "
+              >{{ slotProps.data.inventoryStatus }}</span
+            >
+          </div>
+        </div>
+      </div>
+    </template>
+
+    <template #grid="slotProps">
+      <div class="col-12 md:col-4">
+        <div class="product-grid-item card">
+          <div class="product-grid-item-top">
+            <div>
+              <i class="pi pi-tag product-category-icon"></i>
+              <span class="product-category">{{
+                slotProps.data.category
+              }}</span>
+            </div>
+            <span
+              :class="
+                'product-badge status-' +
+                slotProps.data.inventoryStatus.toLowerCase()
+              "
+              >{{ slotProps.data.inventoryStatus }}</span
+            >
+          </div>
+          <div class="product-grid-item-content">
+            <img
+              src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png"
+              :alt="slotProps.data.name"
+            />
+            <div class="product-name">{{ slotProps.data.name }}</div>
+            <div class="product-description">
+              {{ slotProps.data.description }}
+            </div>
+            <Rating
+              :modelValue="slotProps.data.rating"
+              :readonly="true"
+              :cancel="false"
+            ></Rating>
+          </div>
+          <div class="product-grid-item-bottom">
+            <span class="product-price">${{ slotProps.data.price }}</span>
+            <Button
+              icon="pi pi-shopping-cart"
+              :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'"
+            ></Button>
+          </div>
+        </div>
+      </div>
+    </template>
+  </DataView> -->
 </template>
 
 <script setup lang="ts"></script>
