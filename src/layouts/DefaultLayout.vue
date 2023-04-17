33fb3a2e8aadbfc1e1fffa4d3067a261f99a3078
cosmicKj <kyungjun1213@gmail.com>
2023-04-17 20:33:48 +0900

    Refactoring: Default Layout 코드 정리

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,6 +1,8 @@
 <template>
-  <main class="h-[100svh] sm:bg-gray-200">
-    <div class="sm:max-w-[360px] h-full mx-auto bg-slate-100 shadow-lg">
+  <main class="h-[100svh] flex items-center justify-center sm:bg-gray-200">
+    <div
+      class="w-full sm:w-[360px] h-full sm:max-h-[640px] bg-slate-100 shadow-lg"
+    >
       <div v-if="isAuthReady" class="flex flex-col h-full">
         <RouterView class="overflow-auto flex-1" />
         <RouterView name="GlobalNavbar" />
