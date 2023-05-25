178e514c864dea2f0c80c308fa411d7e7a1c90b0
cosmicKj <kyungjun1213@gmail.com>
2023-05-26 01:21:55 +0900

    Feat: 새친구 등록 UI/UX 및 로직 작업

--- src/views/Home/HomeContainer.vue
+++ src/views/Home/HomeContainer.vue
@@ -4,21 +4,21 @@
       <p>안녕하세요!</p>
       <div class="flex items-center justify-between">
         <span>
-          <strong>{{ userData?.name }}</strong> 선생님
+          <strong>{{ userStore.userData?.name }}</strong> 선생님
         </span>
 
         <span
-          v-if="userData?.provider === 'naver'"
+          v-if="userStore.userData?.provider === 'naver'"
           class="px-2 py-1 bg-[#2db400] text-white uppercase text-sm rounded"
         >
-          {{ userData?.provider }} 연동
+          {{ userStore.userData?.provider }} 연동
         </span>
 
         <span
-          v-else-if="userData?.provider === 'kakao'"
+          v-else-if="userStore.userData?.provider === 'kakao'"
           class="px-2 py-1 bg-[#fee500] text-black uppercase text-sm rounded"
         >
-          {{ userData?.provider }} 연동
+          {{ userStore.userData?.provider }} 연동
         </span>
       </div>
     </header>
@@ -53,7 +53,7 @@
       />
 
       <RouterLink
-        v-if="userData?.role === 'admin'"
+        v-if="userStore.userData?.role === 'admin'"
         class="bg-purple-300 text-purple-700 rounded-lg"
         :to="{ name: 'AdminStudent' }"
       >
@@ -69,7 +69,7 @@
 
     <Button
       rounded
-      class="absolute bottom-4 right-4"
+      class="absolute bottom-4 right-4 shadow-md shadow-slate-400"
       severity="info"
       label="새친구 등록하기"
       icon="pi pi-user-plus"
@@ -79,134 +79,182 @@
     <Dialog
       modal
       v-model:visible="visible"
-      class="w-1/2"
+      class="w-1/3"
+      content-class="overflow-x-hidden p-0"
       header="새친구 등록하기"
       position="bottom"
       :draggable="false"
-      :breakpoints="{ '640px': '90vw' }"
+      :breakpoints="{ '640px': '95vw' }"
+      @update:visible="confirmClose"
     >
       <template #default>
-        <div v-for="(student, idx) in newStudents.body" :key="idx">
-          <div class="flex gap-x-5 mb-2">
-            <div class="flex-1">
-              <p>이름</p>
-              <InputText v-model="student.name" class="w-full" />
-            </div>
+        <TransitionGroup name="list">
+          <div
+            v-for="(student, idx) in newStudents.body"
+            class="m-3 first:mt-0 p-3 rounded-lg bg-slate-100"
+            :key="student.id"
+          >
+            <p class="mb-4 font-bold">{{ idx + 1 }}.</p>
 
-            <div class="flex-1 flex flex-col">
-              <p>성별</p>
-              <div class="flex-1 flex items-center">
-                <RadioButton
-                  v-model="student.gender"
-                  input-id="male"
-                  name="gender"
-                  value="male"
+            <div class="flex gap-x-5 mb-2">
+              <div class="flex-1">
+                <InputText
+                  v-model="student.name"
+                  class="w-full"
+                  :class="{ 'p-invalid': isInvalid(idx, 'name') }"
+                  placeholder="이름"
                 />
-                <label for="male">남자</label>
-
-                <RadioButton
-                  class="ml-2"
-                  v-model="student.gender"
-                  input-id="female"
-                  name="gender"
-                  value="female"
-                />
-                <label for="female">여자</label>
               </div>
-            </div>
-          </div>
 
-          <div class="flex gap-x-5 mb-2">
-            <div class="flex-1">
-              <p>학년</p>
-              <Dropdown
-                v-model="student.grade"
-                class="w-full"
-                :options="GRADE_OPTIONS"
-                option-label="label"
-                option-value="value"
-                placeholder="학년"
-              />
-            </div>
-
-            <div class="flex-1">
-              <p>학급</p>
-              <Dropdown
-                v-model="student.group"
-                class="w-full"
-                placeholder="학급"
-                option-label="label"
-                option-value="value"
-                :options="GROUP_OPTIONS"
-              />
-            </div>
-          </div>
-
-          <div class="flex mb-2">
-            <div class="flex-1">
-              <div class="flex items-center justify-between">
-                <span>생년월일</span>
-                <div class="flex gap-2 items-center justify-between">
-                  <span>나중에 입력</span>
-                  <InputSwitch
-                    v-model="student.birthLater"
-                    @change="toggleBirth(idx)"
+              <div class="flex-1 flex flex-col">
+                <div class="flex-1 flex gap-1 items-center">
+                  <RadioButton
+                    v-model="student.gender"
+                    input-id="male"
+                    name="gender"
+                    value="male"
                   />
+                  <label for="male">남</label>
+
+                  <RadioButton
+                    class="ml-2"
+                    v-model="student.gender"
+                    input-id="female"
+                    name="gender"
+                    value="female"
+                  />
+                  <label for="female">여</label>
                 </div>
               </div>
+            </div>
 
-              <Calendar
-                class="w-full"
-                v-model="student.birth"
-                date-format="yy년 mm월 dd일"
-                placeholder="생년월일을 입력해주세요"
-                :disabled="student.birthLater"
-                showButtonBar
+            <div class="flex gap-x-5 mb-2">
+              <div class="flex-1">
+                <Dropdown
+                  v-model="student.grade"
+                  class="w-full"
+                  :class="{ 'p-invalid': isInvalid(idx, 'grade') }"
+                  :options="GRADE_OPTIONS"
+                  option-label="label"
+                  option-value="value"
+                  placeholder="학년"
+                />
+              </div>
+
+              <div class="flex-1">
+                <Dropdown
+                  v-model="student.group"
+                  class="w-full"
+                  :class="{ 'p-invalid': isInvalid(idx, 'group') }"
+                  placeholder="학급"
+                  option-label="label"
+                  option-value="value"
+                  :options="GROUP_OPTIONS"
+                />
+              </div>
+            </div>
+
+            <div class="flex mb-2">
+              <div class="flex-1">
+                <div class="flex items-center justify-between">
+                  <span>생년월일</span>
+                  <div class="flex gap-2 items-center justify-between">
+                    <span>나중에 입력</span>
+                    <InputSwitch
+                      v-model="student.birthLater"
+                      @change="toggleBirth(idx)"
+                    />
+                  </div>
+                </div>
+
+                <Calendar
+                  touch-u-i
+                  showButtonBar
+                  class="w-full"
+                  v-model="student.birth"
+                  date-format="yy년 mm월 dd일"
+                  placeholder="생년월일을 입력해주세요"
+                  :disabled="student.birthLater"
+                />
+              </div>
+            </div>
+
+            <div class="flex mb-2">
+              <div class="flex-1">
+                <p>초등부 첫 출석 주일</p>
+                <Calendar
+                  touch-u-i
+                  showButtonBar
+                  class="w-full"
+                  v-model="student.registeredAt"
+                  date-format="yy년 mm월 dd일"
+                />
+              </div>
+            </div>
+
+            <div class="flex gap-x-5 mb-2">
+              <div class="flex-1">
+                <InputText
+                  v-model="student.address"
+                  class="w-full"
+                  placeholder="주소"
+                />
+              </div>
+            </div>
+
+            <div class="flex gap-x-5 mb-2">
+              <div class="flex-1">
+                <InputText
+                  v-model="student.phone"
+                  class="w-full"
+                  placeholder="연락처"
+                />
+              </div>
+
+              <div class="flex-1">
+                <InputText
+                  v-model="student.remark"
+                  class="w-full"
+                  placeholder="비고"
+                />
+              </div>
+            </div>
+            <div class="flex gap-2 mt-4 justify-center">
+              <Button
+                outlined
+                size="small"
+                severity="help"
+                label="복사하기"
+                @click="copyNewStudent(idx)"
+              />
+              <Button
+                outlined
+                size="small"
+                severity="danger"
+                label="삭제하기"
+                :disabled="newStudents.body.length === 1"
+                @click="deleteNewStudent(idx)"
               />
             </div>
           </div>
-
-          <div class="flex mb-2">
-            <div class="flex-1">
-              <p>초등부 첫 출석 주일</p>
-              <Calendar
-                class="w-full"
-                v-model="student.registeredAt"
-                date-format="yy년 mm월 dd일"
-                showButtonBar
-              />
-            </div>
-          </div>
-
-          <div class="flex gap-x-5 mb-2">
-            <div class="flex-1">
-              <p>주소</p>
-              <InputText v-model="student.address" class="w-full" />
-            </div>
-          </div>
-
-          <div class="flex gap-x-5 mb-2">
-            <div class="flex-1">
-              <p>연락처</p>
-              <InputText v-model="student.phone" class="w-full" />
-            </div>
-
-            <div class="flex-1">
-              <p>비고</p>
-              <InputText v-model="student.remark" class="w-full" />
-            </div>
-          </div>
-
-          <div class="flex justify-end gap-x-2 mt-4">
-            <Button label="삭제하기" class="p-button-danger p-button-sm" />
-          </div>
-        </div>
+        </TransitionGroup>
       </template>
 
       <template #footer>
-        <Button size="small" severity="success" label="인원 추가" />
-        <Button size="small" severity="help" label="복사하기" />
-        <Button size="small" severity="warning" label="제출하기" />
+        <div class="flex flex-wrap gap-2 justify-end">
+          <Button
+            size="small"
+            severity="success"
+            label="인원 추가"
+            @click="addNewStudent"
+          />
+          <Button
+            size="small"
+            severity="warning"
+            label="제출하기"
+            @click="submitNewStudents"
+          />
+        </div>
       </template>
     </Dialog>
   </section>
@@ -215,18 +263,23 @@
 <script setup lang="ts">
 import HomeMenu from './_partials/HomeMenu.vue';
 
-import { reactive, ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
+import { useMemberStore } from '@/store/member';
 import { useUserStore } from '@/store/user';
+import { useVuelidate } from '@vuelidate/core';
+import { required, helpers } from '@vuelidate/validators';
 import { GRADE_OPTIONS, GROUP_OPTIONS } from '@/constants/common';
-import type { MemberData } from '@/types';
+import type { NewStudent } from '@/types';
 
-const { userData } = useUserStore();
+const memberStore = useMemberStore();
+const userStore = useUserStore();
 
 const visible = ref(false);
-const errors = ref<any[]>([]);
-// const members = ref<any[]>([]);
 
-const initSelectedStudent: MemberData = {
+const id = ref(1);
+
+const initSelectedStudent: NewStudent = {
+  id: 0,
   name: '',
   birth: null,
   birthLater: true,
@@ -243,18 +296,91 @@ const initSelectedStudent: MemberData = {
   remark: '',
 };
 
-const newStudents = reactive<{ body: MemberData[] }>({
-  body: [initSelectedStudent],
+const scroll = () => {
+  setTimeout(() => {
+    const container = document.getElementsByClassName('p-dialog-content');
+
+    container[0].scroll({
+      top: 473 * (newStudents.body.length - 1),
+      left: 0,
+      behavior: 'smooth',
+    });
+  }, 100);
+};
+
+const newStudents = reactive<{ body: NewStudent[] }>({
+  body: [{ ...initSelectedStudent, id: id.value }],
 });
 
-const createNewStudent = (data: MemberData) => Object.assign({}, data);
-
 const addNewStudent = () => {
-  newStudents.body.push(createNewStudent(initSelectedStudent));
+  id.value++;
+  newStudents.body.push({ ...initSelectedStudent, id: id.value });
+  scroll();
+};
+
+const deleteNewStudent = (index: number) => {
+  newStudents.body.splice(index, 1);
+};
+
+const copyNewStudent = (index: number) => {
+  id.value++;
+  newStudents.body.push({ ...newStudents.body[index], id: id.value });
+  scroll();
+};
+
+const rules = {
+  body: {
+    $each: helpers.forEach({
+      name: { required },
+      grade: { required },
+      group: { required },
+    }),
+  },
+};
+
+const $v = useVuelidate(rules, newStudents);
+
+const $errors = computed(() => $v.value.$errors[0]?.$response?.$errors);
+
+const confirmClose = () => {
+  if (confirm('작업한 내용이 모두 사라집니다. 정말 닫으시겠습니까?')) {
+    newStudents.body = [{ ...initSelectedStudent }];
+    $v.value.$reset();
+    return;
+  }
+  visible.value = true;
+};
+
+const submitNewStudents = async () => {
+  try {
+    const isFormCorrect = await $v.value.body.$validate();
+    if (!isFormCorrect) {
+      return;
+    }
+
+    const { church, department } = userStore.userData || {};
+
+    // TODO: newStudents.body에서 id값을 삭제해주고 넣어야한다.
+    if (church && department) {
+      memberStore.createMultiple({
+        church,
+        department,
+        members: newStudents.body,
+      });
+    }
+
+    // createNewStudents();
+    // resetNewStudents();
+    // await getStudents();
+
+    alert('추가되었습니다.');
+  } catch (error) {
+    alert(error);
+  }
 };
 
 const isInvalid = (index: number, key: string) => {
-  if (errors.value && errors.value[index][key].length > 0) {
+  if ($errors.value && $errors.value[index][key].length > 0) {
     return true;
   }
   return false;
@@ -275,3 +401,19 @@ const toggleBirth = (index: number) => {
   // }
 };
 </script>
+
+<style scoped>
+.p-dialog .p-dialog-footer button {
+  margin: unset;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease-out;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+</style>
