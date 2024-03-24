b51872ed93b8fbfab602e8e06b77ff79524a3ad6
cosmickj <kyungjun1213@gmail.com>
2024-03-24 22:05:23 +0900

    feat: update vue3 version

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  root: true,
-  env: { node: true },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
-  rules: [],
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
-import { screens } from 'tailwindcss/defaultTheme';
+import { screens } from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       screens: {
         xs: '321px',
-        ...screens,
+        ...screens
       },
       flex: {
         2: '2 2 0%',
-        3: '3 3 0%',
-      },
-    },
+        3: '3 3 0%'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
