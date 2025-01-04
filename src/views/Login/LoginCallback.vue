f255b8ccdd150b2ff0fc348ed9abc2eebd3eca39
cosmickj <kyungjun1213@gmail.com>
2025-01-05 00:35:35 +0900

    ♻️ Add return statement to prevent unexpected behavior

--- src/views/Login/LoginCallback.vue
+++ src/views/Login/LoginCallback.vue
@@ -24,7 +24,7 @@ onMounted(async () => {
 
     await userStore.fetchSingle({ uid });
 
-    router.replace({ name: 'HomeView' });
+    return router.replace({ name: 'HomeView' });
   } catch (error) {
     throw Error((error as Error).message);
   } finally {
